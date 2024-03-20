438e43762c97a4ff2e3e38e2cb396df574b2a46c
mav9rick <koil-3@yandex.ru>
2024-03-20 11:27:30 +0500

    Добавлены комментарии

--- task-27e/mainwindow.h
+++ task-27e/mainwindow.h
@@ -7,29 +7,30 @@
 #include <QPushButton>
 class mainwindow:public QWidget
 {
-    Q_OBJECT
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
 protected:
     QTextCodec *codec;
-    QFrame *frame;
-    QLabel *inputLabel;
-    QLineEdit *inputEdit;
-    QLabel *outputLabel;
-    QLineEdit *outputEdit;
-    QPushButton *nextButton;
-    QPushButton *exitButton;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
-    mainwindow(QWidget *parent = 0);
+    mainwindow(QWidget *parent = 0); // конструктор
 public slots:
-    void begin();
-    void calc();
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
 };
-class StrValidator:public QValidator
+class StrValidator:public QValidator // класс компонента проверки ввода
 {
 public:
     StrValidator(QObject *parent):QValidator(parent){}
     virtual State validate(QString &str,int &pos)const
     {
-        return Acceptable;
+        return Acceptable; // метод всегда принимает вводимую строку
     }
 };
+
 #endif
